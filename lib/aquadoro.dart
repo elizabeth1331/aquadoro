20b12cac18ffe6aa043bc7a74d47b027e0ed4d28
elizabethmendoza1509@gmail.com <elizabethmendoza1509@gmail.com>
2020-09-12 14:34:28 -0500

    AcuadoroStack, se centro texto

--- lib/aquadoro.dart
+++ lib/aquadoro.dart
@@ -19,8 +19,12 @@ class Aquadoro extends StatefulWidget {
 }
 
 class _AquadoroState extends State<Aquadoro> {
+  String tipoActividad = "focus";
+  String tiempoPantalla= "12:00";
+
   @override
   Widget build(BuildContext context) {
+    double ancho= MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -30,23 +34,54 @@ class _AquadoroState extends State<Aquadoro> {
       body: Stack(
         children: [
           Container(color: Colors.cyan[600]),
-          Column(
-              children: [
-                Center(child: Text('Hola Xd', style: TextStyle(
-                  fontSize: 25,color: Colors.blueGrey[50]),
-                )),
-                Center(child: Text('Actividad ${widget.actividad}', style: TextStyle(
-                  fontSize: 25,color: Colors.blueGrey[50]),)),
-                Center(child: Text('Tiempo de concentracion ${widget.tConcentracion}', style: TextStyle(
-                  fontSize: 25,color: Colors.blueGrey[50]),)),
-                Center(child: Text('Tiempo de descanso ${widget.tDescanso}', style: TextStyle(
-                  fontSize: 25,color: Colors.blueGrey[50]),)),
-              ],
-          ),
+          _aquadoroStack(ancho),
+        
         ],
 
       ),
 
+    );
+  }
+  Widget _aquadoroStack(double ancho){
+    return Stack(
+      children: [
+        Container(
+          width: 357,
+          height: 357,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(image: AssetImage('assets/Acuadoro.png'), fit: BoxFit.cover),
+          ),
+
+        ),
+
+        Positioned(
+          top: 160,
+          left: ancho * 0.25,
+                  child: Container(
+                      height: 100,
+                      width: 170,
+                      child: Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              tipoActividad,
+                              style:  TextStyle(fontSize: 25, color: Colors.blueGrey[100]),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              tiempoPantalla, 
+                              style:  TextStyle(fontSize: 25, color: Colors.blueGrey[100]),
+                            ),
+                          )
+                        ]
+                   ),
+                 ),
+               ),
+
+      ],
     );
   }
 }
